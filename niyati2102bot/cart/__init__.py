8ba752c7acc134974a70ab96133d783ce5c757c5
MangleshQB <manglesh@quantumbot.co.in>
2023-08-02 11:25:58 +0530

    Mycode

--- niyati2102bot/cart/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import add_to_cart, checkout, go_to_cart, delete_product
--- niyati2102bot/credentials.py
+++
@@ -1,3 +0,0 @@
-# Do not remove these 2 lines:
-BOT_TOKEN = '5776106083:AAHiulaiOjRR9qVAONpSnK3oE1FKg15z_i4'  # You should consider using env variables or a secret manager for this.
-APP_NAME = 'niyati2102bot'
